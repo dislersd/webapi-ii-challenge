3ade30a486f2f6314841dbecbc991615668d6eb3
dislersd <dislersd@gmail.com>
2019-03-19 14:45:52 -0700

    created put route

--- posts-router.js
+++ posts-router.js
@@ -28,8 +28,12 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
-    const post = await db.add(req.body);
-    res.status(201).json(post);
+    if (req.body.title && req.body.contents) {
+      const post = await db.insert(req.body);
+      res.status(201).json(post);
+    } else {
+      res.status(400).json({ message: "please enter title and content" });
+    }
   } catch (error) {
     res.status(500).json({
       message: "Error adding the hub"
@@ -48,6 +52,25 @@ router.delete("/:id", async (req, res) => {
   } catch (error) {
     res.status(500).json({
       message: "error deleting"
+    });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const change = req.body;
+    const updated = await db.update(id, change);
+    if (updated) {
+      res.status(200).json({ mesage: "post updated", updated: updated });
+    } else {
+      res.status(404).json({
+        message: "post not found"
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "error updating"
     });
   }
 });
